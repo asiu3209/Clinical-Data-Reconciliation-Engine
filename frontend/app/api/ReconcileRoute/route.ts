4369372bd82439f7b2b62dcc256a9f395d7f7e8e
Aaron Siu <99273246+asiu3209@users.noreply.github.com>
2026-03-17 21:54:42 -0400

    Call backend api

--- frontend/app/api/ReconcileRoute/route.ts
+++ frontend/app/api/ReconcileRoute/route.ts
@@ -1,5 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest){
-    
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+
+  const response = await fetch("http://localhost:8000/reconcile/medication", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  const result = await response.json();
+
+  return NextResponse.json(result);
 }
--- frontend/app/api/ValidateRoute/route.ts
+++ frontend/app/api/ValidateRoute/route.ts
@@ -1,3 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(request: NextRequest) {}
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+
+  const response = await fetch("http://localhost:8000/validate/data-quality", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  const result = await response.json();
+
+  return NextResponse.json(result);
+}
